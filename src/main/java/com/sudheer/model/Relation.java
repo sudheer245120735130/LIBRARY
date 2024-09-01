102c9e35ce5ca39419fcd4b4edc7a4626ffa8821
sudheer <sudheerbudidi@gmail.com>
2024-09-02 00:39:28 +0530

    Update Relation.java

--- src/main/java/com/sudheer/model/Relation.java
+++ src/main/java/com/sudheer/model/Relation.java
@@ -1,5 +1,12 @@
 package com.sudheer.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Relation {
+	@Autowired
+	 
+	
+	private Book B;
+	private Author A;
 
 }
